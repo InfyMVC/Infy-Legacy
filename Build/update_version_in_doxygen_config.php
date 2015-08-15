ea67c5c46fde69056c72c674f78757a41ce173d4
YannickFricke <frickx94@googlemail.com>
2015-08-15 13:52:01 +0200

    [Travis] Updated build mechanism

---
+++ Build/update_version_in_doxygen_config.php
@@ -0,0 +1,30 @@
+<?php
+
+$handle = fopen("Infy/Infy.php", "r");
+
+$version = null;
+
+if ($handle)
+{
+    while (($line = fgets($handle)) !== false)
+    {
+        preg_match('/private static \$_version = "(?<version>.*)";/', $line, $matches);
+
+        if (count($matches) != 0)
+        {
+            $version = $matches["version"];
+        }
+    }
+
+    fclose($handle);
+}
+else
+{
+    echo "Can't open '../Infy/Infy.php'. Please check your file permissions";
+}
+
+$doxygenConfig = file_get_contents("../doxygen.config");
+
+$doxygenConfig = preg_replace('/PROJECT_NUMBER         =.*/', 'PROJECT_NUMBER         = '.$version, $doxygenConfig);
+
+file_put_contents("../doxygen.config", $doxygenConfig);
